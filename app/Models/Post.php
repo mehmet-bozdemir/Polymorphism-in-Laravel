2195acf7090dde3ddb75125dc98422f0eab2f78a
A.T <a.t.@192.168.2.70>
2020-10-13 13:30:36 +0200

    polymorphic relationships in Laravel

---
+++ app/Models/Post.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $guarded = [];
+    use HasFactory;
+
+    public function image(){
+        return $this->morphOne(Image::class, 'imageable');
+    }
+
+    public function comments(){
+        return $this->morphMany(Comment::class, 'commentable');
+    }
+
+    public function tags(){
+        return $this->morphToMany(Tag::class, 'taggable');
+    }
+}
